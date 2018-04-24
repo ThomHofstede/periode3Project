6ac58720f41e866008fbbc698628196b200d6429
Mats <matsjongman@hotmail.nl>
2018-04-24 13:17:28 +0200

    Schermen gepushed

---
+++ KBS/src/Schermen/ProjectTZT.java
@@ -0,0 +1,33 @@
+
+package project.tzt;
+
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+
+public class ProjectTZT {
+
+
+    public static void main(String[] args) {
+        Welkomstscherm welkomstscherm = new Welkomstscherm();
+        welkomstscherm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        welkomstscherm.setVisible(true);
+    }
+    /*private JButton login, registreer;
+    
+    public Startscherm(){
+        setTitle("TZT Post");
+        setLayout(new FlowLayout());
+        setSize(1200,700);
+        
+        login = new JButton("Login");
+        login.addActionListener(this);
+        this.add(login);
+        registreer = new JButton("Registreer");
+        registreer.addActionListener(this);
+        this.add(registreer);
+        
+        this.setVisible(true);
+    } */
+}
